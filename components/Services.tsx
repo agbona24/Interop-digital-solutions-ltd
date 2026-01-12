fe290f7d517e21feb3627701918ecb529b1e0c82
agbonaazeez <softlineazeez123@gmail.com>
2026-01-12 13:53:00 +0100

    Update

--- components/Services.tsx
+++ components/Services.tsx
@@ -32,13 +32,14 @@ export default function Services() {
       { threshold: 0.1 }
     );
 
-    if (sectionRef.current) {
-      observer.observe(sectionRef.current);
+    const currentSection = sectionRef.current;
+    if (currentSection) {
+      observer.observe(currentSection);
     }
 
     return () => {
-      if (sectionRef.current) {
-        observer.unobserve(sectionRef.current);
+      if (currentSection) {
+        observer.unobserve(currentSection);
       }
     };
   }, []);
